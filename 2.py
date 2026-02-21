82b152ca7a5bca564e7c5f06b06a1aa5772697dc
tyagisuraj <tyagisuraj678@gmail.com>
2026-02-21 14:04:37 +0530

    Add getMinMax method to find min and max in array
    Implemented a method to find minimum and maximum values in an array.

---
+++ 2.py
@@ -0,0 +1,20 @@
+# Given an array arr[]. Your task is to find the minimum and maximum elements
+# in the array.
+# Examples:
+# Input: arr[] = [1, 4, 3, 5, 8, 6]
+# Output: [1, 8]
+# Explanation: minimum and maximum elements of array are 1 and 8
+class Solution:
+    def getMinMax(self, arr):
+        # code hdef findMinMax(arr):
+    
+        minimum = arr[0]
+        maximum = arr[0]
+    
+        for i in range(1, len(arr)):
+            if arr[i] < minimum:
+                minimum = arr[i]
+            if arr[i] > maximum:
+                maximum = arr[i]
+    
+        return [minimum, maximum]
