f9d9582ffdef91bfe4e9c6c6a9e45c184ac7716b
Suraj Tyagi <tyagisuraj678@gmail.com>
2026-02-23 10:47:38 +0530

    Create 5th.pyGiven an array arr[] of positive integers. Return true if all the array elements are  palindrome otherwise, return false.

---
+++ 3/5th.py
@@ -0,0 +1,22 @@
+# Given an array arr[] of positive integers. Return true if all the array elements are
+# palindrome otherwise, return false.
+# Examples:
+# Input: arr[] = [111, 222, 333, 444, 555]
+# Output: true
+# Explanation:
+# arr[0] = 111, which is a palindrome number.
+# arr[1] = 222, which is a palindrome number.
+# arr[2] = 333, which is a palindrome number.
+# arr[3] = 444, which is a palindrome number.
+# arr[4] = 555, which is a palindrome number.
+# As all numbers are palindrome so This will return true.
+# Input: arr[] = [121, 131, 20]
+# Output: false
+# Explanation: 20 is not a palindrome hence the output is false
+class Solution:
+    def PalinArray(self, arr):
+        for num in arr:
+            s = str(num)
+            if s != s[::-1]:
+                return False
+        return True
