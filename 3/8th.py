9e17f021eaf978f17760f65db1d11dab971679b0
Suraj Tyagi <tyagisuraj678@gmail.com>
2026-02-23 10:59:16 +0530

    You are given an m x n integer matrix matrix with the following two properties:  Each row is sorted in non-decreasing order.  The first integer of each row is greater than the last integer of the previous row.  Given an integer target, return true if target is in matrix or false otherwise.  You must write a solution in O(log(m * n)) time complexity

---
+++ 3/8th.py
@@ -0,0 +1,34 @@
+# You are given an m x n integer matrix matrix with the following two properties:
+
+# Each row is sorted in non-decreasing order.
+# The first integer of each row is greater than the last integer of the previous row.
+# Given an integer target, return true if target is in matrix or false otherwise.
+
+# You must write a solution in O(log(m * n)) time complexity
+class Solution:
+    def searchMatrix(self, matrix, target):
+        if not matrix or not matrix[0]:
+            return False
+        
+        m = len(matrix)
+        n = len(matrix[0])
+        
+        left = 0
+        right = m * n - 1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            
+            row = mid // n
+            col = mid % n
+            
+            value = matrix[row][col]
+            
+            if value == target:
+                return True
+            elif value < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return False
